7e3308ca3c34b5052c0ec5d69265b7249f36c0eb
Kamila Hantova <kamila.hantova@zf.com>
2023-01-23 11:17:47 +0100

    improve errors in main

--- main.py
+++ main.py
@@ -20,33 +20,33 @@ app = Flask(__name__)  # __name__ jmeno instance
 ledger = Ledger()
 
 
-ledger.add_user("Petr")
-ledger.transaction("Pavel", "Petr", 15.0)
-ledger.transaction("Petr", "Jakub", 2.5)
+#ledger.add_user("Petr")
+#ledger.transaction("Pavel", "Petr", 15.0)
+#ledger.transaction("Petr", "Jakub", 2.5)
 
 
-@app.get("/user/<name>/")  # get user by name
+@app.get("/user/<name>/")
 def get_user(name):
     user = ledger.get_user(name)
     return json.dumps(user.__dict__, indent=2)
 
 
-@app.post("/add")  # add new user with name
+@app.post("/add")
 def add_user():
-    if request.is_json:  # chci aby pozadavek mel telo Json
-        data = request.get_json()  #predelej request do json
-        Ledger.add_user(data["user"])
-        return 200
-    return {"error": "Name is not unique or request has wrong format"}, 418
+    if request.is_json:
+        data = request.get_json()
+        ledger.add_user(data["user"])
+        return "done"
+    return {"error": "Name is not unique or request has wrong format"}
 
 
 @app.post("/transaction")
 def add_transaction():
     if request.is_json:
         data = request.get_json()
-        Ledger.transaction(data["veritel"], data["dluznik"], data["castka"])
-        return 200
-    return {"error": "Request has wrong format"}, 418
+        ledger.transaction(data["veritel"], data["dluznik"], float(data["castka"]))
+        return "done"
+    return {"error": "Request has wrong format"}
 
 
 app.run(host='0.0.0.0', port=81)
--- users.py
+++ users.py
@@ -17,54 +17,51 @@ class User:
 
     def set_dluzi(self, name: str, amount: float):
         if name not in self.dluzi:
-            self.dluzi[name]=0
-        self.dluzi[name] += amount   
-        self.suma +=  amount
+            self.dluzi[name] = 0
+        self.dluzi[name] += amount
+        self.suma += amount
 
     def get_dluzi_mu(self) -> dict:
         return self.dluzi_mu
 
     def set_dluzi_mu(self, name: str, amount: float):
         if name not in self.dluzi_mu:
-            self.dluzi_mu[name]=0
-        self.dluzi_mu[name] += amount    
-        self.suma -= amount 
+            self.dluzi_mu[name] = 0
+        self.dluzi_mu[name] += amount
+        self.suma -= amount
 
     def get_suma(self, suma: float):
         self.suma = suma
 
 
-class Ledger:  # definuje tridu banka s uzivately
+class Ledger:
     _users: dict[str, User]
 
     def __init__(self):
-        self._users = dict()  # vytvor prazdny dict - konstruktor
+        self._users = dict()
 
-    #GET user
-    def get_user(self, name: str) -> User:  # tohle chce dopsat
+    # GET user
+    def get_user(self, name: str) -> User:
         if name not in self._users:
             raise
         return self._users[name]
 
-    #POST new user
+    # POST new user
     def add_user(self, name: str):
         if name in self._users:
             raise
         self._users[name] = User(name)
 
-    #POST transaction (veritel, dluznik, castka)
+    # POST transaction (veritel, dluznik, castka)
     def transaction(self, creditor_name: str, debtor_name: str, amount: float):
         if debtor_name not in self._users:
             self.add_user(debtor_name)
         if creditor_name not in self._users:
             self.add_user(creditor_name)
-       
-        creditor = self.get_user(creditor_name)
-        creditor.set_dluzi_mu(debtor_name,amount)
-        
-        debtor = self.get_user(debtor_name)
-        debtor.set_dluzi(creditor_name,amount)
-        # nezapomen ze to je i naopak kdo dluzi tomu je dluzny 
-        # musi se pridavat ne prepisovat -> get pak set
 
-    
+        creditor = self.get_user(creditor_name)
+        creditor.set_dluzi_mu(debtor_name, amount)
+
+        debtor = self.get_user(debtor_name)
+        debtor.set_dluzi(creditor_name, amount)
+
